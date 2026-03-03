d30a4888f5c091ec85244556f5f84aa1926eb5da
TYLER UNATHI DHLODHLO <tylerdhlodhlo769@gmail.com>
2026-03-03 20:22:39 +0200

    Add files via upload

---
+++ Details.py
@@ -0,0 +1,10 @@
+#Ask the user to enter their name
+name = input("please enter your name: ")
+#Ask the user to enter their age
+age = input("please enter your age")
+#Ask the user to enter their house nmber
+house_number = input("please enter your house number")
+#Ask the user to enter their street name
+street_name = input("please enter your street name")
+#print the details of the user back to them in a sentence
+print("Hello " + name + ", you are " + age + " years old, and live at house number " + house_number + " on " + street_name)
---
+++ conversion.py
@@ -0,0 +1,21 @@
+#declare the following variables num1 and num2 and num 3 and string1
+num1  = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+#convert num1 to an integer
+num1_int = int(num1)
+#convert num2 to a float
+num2_float = float(num2)
+#convert num3 to a string
+num3_string = str(num3)
+#convert string1 to an integer
+string1_int = int(string1)
+#print the results
+print("num1 as an integer" + str(num1_int))  
+print("num2 as a float" + str(num2_float))
+print("num3 as a string" + num3_string)
+print("string1 as an integer" + str(string1_int))
+
+
+
